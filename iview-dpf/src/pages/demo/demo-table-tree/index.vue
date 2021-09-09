7658a13c48dfa52d7801924ab49fa7811f405b30
董鹏飞 <dongpf@bingosoft.net>
2021-09-09 14:02:57 +0800

    表格父子联动

--- iview-dpf/src/pages/demo/demo-table-tree/index.vue
+++ iview-dpf/src/pages/demo/demo-table-tree/index.vue
@@ -10,6 +10,7 @@
             return {
                 parentRow: null, // 对应父节点的数据
                 tongjiRow: [], // 记录同级节点的数据
+                allChildData: [], // 该节点所有子级节点
                 columns16: [
                     {
                         title: 'Name',
@@ -112,10 +113,9 @@
             handleChange (row, flag) {
                 if (flag) { // 逻辑1，如果该节点是选中
                     this.handleTrue(this.data12, row, flag)
-                } else { // 逻辑2，如果该节点是选中
-                    // 先判断同级的其他子结点是否有选中的状态
-                    let changeParentFlag = this.handleChangeParent(row)
-                    this.handleFalse(this.data12, row, flag, changeParentFlag)
+                } else { // 逻辑2，如果该节点是取消选中
+                    this.handleFalse(this.data12, row, flag) // 取消选中他的所有父节点
+                    this.handleFalseChild(this.data12, row, flag) // 取消选中他所有的子结点
                 }
             },
             // 树节点选中，找父节点，一直到根节点，都变成选中
@@ -141,7 +141,9 @@
                 }
             },
             // 树节点取消选中
-            handleFalse (dataArr, row, flag, changeParentFlag) {
+            handleFalse (dataArr, row, flag) {
+                // 先判断同级的其他子结点是否有选中的状态
+                let changeParentFlag = this.handleChangeParent(row)
                 for (let i=0; i<dataArr.length; i++) {
                     let item = dataArr[i]
                     if (item.id == row.parentId) { // 先记录父节点的数据
@@ -151,13 +153,13 @@
                         this.$set(item, '_checked', flag)
                         // 当前节点改变后,如果存在父节点，并且父节点也要改变，则去改边父节点的数据，以此类推直到根节点结束
                         if (item.parentId != '0' && changeParentFlag) { // 开启下一轮循环，把父节点当成当前数据
-                            this.handleFalse(this.data12, this.parentRow, flag, changeParentFlag)
+                            this.handleFalse(this.data12, this.parentRow, flag)
                         }
                         return
                     }
                     // 如果循环到当前层级最后一条数据仍没找到该节点，则找他的子级，然后回调
                     if (item.children && item.children.length) {
-                        this.handleFalse(item.children, row, flag, changeParentFlag)
+                        this.handleFalse(item.children, row, flag)
                     }
                 }
             },
@@ -188,7 +190,47 @@
                         this.handleForTree(item.children, row) // 有children的话再次回调
                     }
                 }
-            }
+            },
+            // 取消选中所有子结点
+            handleFalseChild (dataArr, row, flag) {
+                this.allChildData = [] // 先查询所有子级节点，然后把数据源中对应的子级节点的状态都变成false
+                this.getAllChildData(dataArr, row) // 你写这的时候就把这个方法换成你的接口，直接让this.allChildData等于后台返回的数据
+                this.handleForTreeChild(dataArr, flag) // 改变树元数据所有子结点的状态
+            },
+            // 获取该节点所有子结点的数据，这个方法就相当于你那后台接口，根据父节点查询所有子级节点
+            getAllChildData (dataArr, row) {
+                dataArr.forEach(item => {
+                    if (item.id == row.id) { // 找到当前节点
+                        if (item.children && item.children.length) {
+                            this.allChildData = this.allChildData.concat(item.children)
+                            item.children.forEach(item2 => { // 再查子结点的子结点
+                                this.getAllChildData(dataArr, item2)
+                            })
+                        }
+                    }
+                    // 如果循环到当前层级最后一条数据仍没找到该节点，则找他的子级，然后回调
+                    if (item.children && item.children.length) {
+                        this.getAllChildData(item.children, row) // 有children的话再次回调
+                    }
+                })
+            },
+            // 循环树数据，用于把所有的子结点都变成false
+            handleForTreeChild (dataArr, flag) {
+                for (let i=0; i<dataArr.length; i++) { // 循环树形数据
+                    let item = dataArr[i] // 每条数据
+                    for (let j=0; j<this.allChildData.length; j++) { // 循环所有子结点数据
+                        let item2 = this.allChildData[j]
+                        if (item.id == item2.id) { // 在树型结构数据中找到当前节点数据
+                            this.$set(item, '_checked', flag) // 改变状态
+                            break // 跳出循环进行下一次
+                        }
+                    }
+                    // 如果循环到当前层级最后一条数据仍没找到该节点，则找他的子级，然后回调
+                    if (item.children && item.children.length) {
+                        this.handleForTreeChild(item.children, flag) // 有children的话再次回调
+                    }
+                }
+            },
         }
     }
 </script>
