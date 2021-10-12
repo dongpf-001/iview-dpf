6a86bf77b9b16b6e1b8793122a85e229182993e8
董鹏飞 <dongpf@bingosoft.net>
2021-10-12 09:03:32 +0800

    锚点实现单据/iview锚点有bug

--- iview-dpf/src/pages/demo/demo-auchor-bill/index.vue
+++ iview-dpf/src/pages/demo/demo-auchor-bill/index.vue
@@ -1,6 +1,67 @@
 <template>
-    <div>
-        单据
+    <div class="my-bill">
+        <div class="auchor-title">
+            <Anchor ref="anchorLink" @on-select="handleSelect" show-ink :affix="false" container=".auchor-body">
+                <AnchorLink href="#demo1" title="demo1"/>
+                <AnchorLink href="#demo2" title="demo2"/>
+                <AnchorLink href="#demo3" title="demo3"/>
+                <AnchorLink href="#demo4" title="demo4"/>
+            </Anchor>
+        </div>
+        <div class="auchor-body">
+            <div id="demo1">
+                <bingo-shrink-card height="400">
+                    <span slot="icon">
+                        <Icon type="md-close"/>
+                    </span>
+                    <div style="height: 100%">
+                        ------------one
+                        <Card class="ivu-mb-8" style="height: calc(100% - 25px)">
+                            模块1
+                        </Card>
+                    </div>
+                </bingo-shrink-card>
+            </div>
+            <div id="demo2">
+                <bingo-shrink-card height="400">
+                    <span slot="icon">
+                        <Icon type="md-close"/>
+                    </span>
+                    <div style="height: 100%">
+                        ------------two
+                        <Card class="ivu-mb-8" style="height: calc(100% - 25px)">
+                            模块2
+                        </Card>
+                    </div>
+                </bingo-shrink-card>
+            </div>
+            <div id="demo3">
+                <bingo-shrink-card height="400">
+                    <span slot="icon">
+                        <Icon type="md-close"/>
+                    </span>
+                    <div style="height: 100%">
+                        ------------three
+                        <Card class="ivu-mb-8" style="height: calc(100% - 25px)">
+                            模块3
+                        </Card>
+                    </div>
+                </bingo-shrink-card>
+            </div>
+            <div id="demo4">
+                <bingo-shrink-card height="400">
+                    <span slot="icon">
+                        <Icon type="md-close"/>
+                    </span>
+                    <div style="height: 100%">
+                        ------------fore
+                        <Card class="ivu-mb-8" style="height: calc(100% - 25px)">
+                            模块4
+                        </Card>
+                    </div>
+                </bingo-shrink-card>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -18,8 +79,27 @@
         mounted () {
         },
         methods: {
+            handleSelect (href) {
+                this.$nextTick(() => {
+                    this.$refs.anchorLink.currentLink = href
+                })
+            }
         }
     };
 </script>
 <style lang="less" scoped>
+    .my-bill {
+        height: 100%;
+        overflow: hidden;
+    }
+    .auchor-body {
+        width: 80%;
+        height: 100%;
+        overflow: auto;
+        float: left;
+    }
+    .auchor-title {
+        float: right;
+        margin-right: 30px;
+    }
 </style>
